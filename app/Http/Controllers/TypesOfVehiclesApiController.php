97a714e273f851b82527957ad305e2970fe42b5b
Shelsy <shelsyangelik@gmail.com>
2024-04-26 15:54:39 -0500

    practica CRUD api

---
+++ app/Http/Controllers/TypesOfVehiclesApiController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypesOfVehicle;
+use Illuminate\Http\Request;
+
+class TypesOfVehiclesApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datos = TypesOfVehicle::all();
+        return response()->json($datos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $datos = TypesOfVehicle::create($request->all());
+        return response()->json($datos,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $datos = TypesOfVehicle::findOrFail($id);
+        return response()->json($datos);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $datos = TypesOfVehicle::findOrFail($id);
+
+        $datos->update($request->all());
+        return response()->json($datos,200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        TypesOfVehicle::findOrFail($id)->delete();
+        return response()->json(null,204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TypesOfVehiclesApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/type-of-vehicles',[TypesOfVehiclesApiController::class, 'index']);
+Route::get('/type-of-vehicles/{id}',[TypesOfVehiclesApiController::class, 'show']);
+Route::put('/type-of-vehicles/{id}',[TypesOfVehiclesApiController::class, 'update']);
+Route::delete('/type-of-vehicles/{id}',[TypesOfVehiclesApiController::class, 'destroy']);
+
+
